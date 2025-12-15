449415ed8b130f353bdc81a4d3b5501ebf57387b
Mateus <matt_ufo2@hotmail.com>
2025-12-15 12:11:52 -0300

    feat: Adicionar enum GeneroFilme e integração com MySQL via Entity Framework Criada enumeração GeneroFilme com gêneros pré-definidos Adicionada propriedade TituloOriginal na classe Filme Implementado FilmeContext com Entity Framework Core Configurada conexão MySQL no Program.cs usando DbContext Ajustadas validações com DataAnnotations

---
+++ FilmesApi/Models/Enums/GeneroFilme.cs
@@ -0,0 +1,22 @@
+﻿namespace FilmesApi.Models.Enums
+{
+    public enum GeneroFilme : int
+    {
+
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        FiccaoCientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13,
+        Animacao = 14,
+        Crime = 15,
+    }
+}
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FilmesApi.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesApi.Models;
 
@@ -10,9 +11,12 @@ public class Filme
     [MaxLength(50, ErrorMessage = "O titulo nao deve exceder 50 caracteres")]
     public string Titulo { get; set; }
 
+    [Required]
+    [MaxLength(50, ErrorMessage = "O titulo nao deve exceder 50 caracteres")]
+    public string TituloOriginal { get; set; }
+
     [Required(ErrorMessage = "O gênero é obrigatório")]
-    [MaxLength(30, ErrorMessage = "O gênero nao deve exceder 30 caracteres")]
-    public string Genero { get; set; }
+    public GeneroFilme Genero { get; set; }
 
     [Required(ErrorMessage = "O titulo é obrigatório")]
     [MaxLength(30, ErrorMessage = "O diretor nao deve exceder 30 caracteres")]
